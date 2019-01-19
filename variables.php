b82adfa220a437f3cce8b8435b4150084b884680
Savannah Mitchell <46454373+mitc0304@users.noreply.github.com>
2019-01-19 18:23:42 -0500

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,41 @@
+<!--
+| 1 |
++---+
+Declare the variable named language and 
+assign it with value PHP.-->
+
+
+<?php 
+
+$variable = "PHP";
+
+?>
+
+<!--
+Combine (chain or concatenate) language 
+with the string literal to create the expression 
+which will output the sentence: 
+Welcome to PHP!. 
+
+Declare the variable greeting, 
+assign it with the created expression.
+-->
+
+
+<?php
+
+echo "Welcome to $variable!";
+
+$greeting = "Welcome to $variable!";
+
+?>
+
+<!--
+Print greeting.
+-->
+
+<?php
+
+echo $greeting;
+
+?>
